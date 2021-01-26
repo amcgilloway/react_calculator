b005bab6a477534bdc5ac507c4450a189be228db
Alastair McGilloway <alastairmcgillowaY@hotmail.com>
2021-01-26 15:22:16 +0000

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,95 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Calculator from './components/Calculator';
 import './App.css';
 
 function App() {
+
+  const [msg] = useState("Calculator"); 
+  const [previousTotal, setPreviousTotal] = useState(0); 
+  const [runningTotal , setRunningTotal] = useState(0); 
+  const [previousOperator, setPreviousOperator] = useState(null); 
+  const [newTotal, setNewTotal] = useState(true); 
+
+  const numberClick = (number) => {
+    if (runningTotal === 0 || newTotal){
+      setRunningTotal("");
+      setNewTotal(false);
+    }
+
+    setRunningTotal(parseFloat("" + runningTotal + number));
+  }
+
+  const clearClick = () => {
+    if (runningTotal === 0) {
+      setPreviousOperator(null);
+      setPreviousTotal(null);
+    }
+    setRunningTotal(0);
+  }
+
+  const operatorClick = (operator) => {
+    // if there was a previous operator recorded as having been clicked, perform
+      // the operation for the previous operator
+      if (previousTotal && previousOperator) {
+        switch (previousOperator) {
+          case "+":
+            add(runningTotal);
+            break;
+          case "-":
+            subtract(runningTotal);
+            break;
+          case "*":
+            multiply(runningTotal);
+            break;
+          case "/":
+            divide(runningTotal);
+            break;
+        }
+      }
+
+      // if the 'equals' button was clicked, clear the previous operator, otherwise
+      // record what the previous operator was
+      if (operator === "=") {
+        setPreviousOperator(null);
+      } else {
+        setPreviousOperator(operator);
+      }
+      // replace the previous total with the current running total and flag that a
+      // new total has been calculated
+
+      setPreviousTotal(runningTotal);
+      setNewTotal(true);
+  }
+
+  const add = (number) => {
+    setRunningTotal(parseFloat(previousTotal) + parseFloat(number));
+  }
+
+  const subtract = (number) => {
+    setRunningTotal(parseFloat(previousTotal) - parseFloat(number));
+  }
+
+  const multiply = (number) => {
+    setRunningTotal(parseFloat(previousTotal) * parseFloat(number));
+  }
+
+  const divide = (number) => {
+    setRunningTotal(parseFloat(previousTotal) / parseFloat(number));
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <h1 data-testid="msg">{ msg }</h1>
+    <div className="calculator">
+      <div data-testid="runnning-total" className="display">{ runningTotal }</div>
+      <Calculator 
+      handleNumber={numberClick} 
+      handleOperator={operatorClick} 
+      handleClear={clearClick}
+      />
     </div>
+  </div>
   );
 }
 
